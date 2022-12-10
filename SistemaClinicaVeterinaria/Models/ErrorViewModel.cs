4eb6a549534174ed3625d0b8eeb63bac5f08289b
Jefferson Santos <jefferson.amsantos@hotmail.com>
2022-12-10 15:41:16 -0300

    Projeto iniciado

---
+++ SistemaClinicaVeterinaria/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaClinicaVeterinaria.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
